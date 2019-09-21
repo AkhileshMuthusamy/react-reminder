f93abfd8bfbc3c19e341bc2dea4be1c0d048a7da
Akhilesh <AkhileshM@outlook.in>
2019-09-21 15:44:07 +0530

    Added code to open and close drawer

--- src/components/Layouts/NavBar/NavBar.style.js
+++ src/components/Layouts/NavBar/NavBar.style.js
@@ -2,6 +2,8 @@ import { withStyles } from '@material-ui/core/styles';
 
 const color = 'rgba(255, 255, 255, 0.7)';
 
+const drawerWidth = 200;
+
 /**
  * Overriding default style of particular drawer
  */
@@ -10,6 +12,27 @@ export default withStyles(theme => ({
     background: '#19212b',
     '& *': {
       color
+    }
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    overflowX: 'hidden',
+    overflow: 'hidden'
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    overflowX: 'hidden',
+    overflow: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(7)
     }
   }
 }));

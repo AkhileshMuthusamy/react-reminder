a85df5dfda01744ea2e37d0fa088f9d55a33f842
Akhilesh <AkhileshM@outlook.in>
2019-09-21 15:44:47 +0530

    Added ListItem to navbar

--- src/components/Layouts/NavBar/NavBar.js
+++ src/components/Layouts/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import clsx from 'clsx';
 
-import { Drawer, Divider, IconButton } from '@material-ui/core';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import { Dashboard, DesktopWindows, Work } from '@material-ui/icons';
 
 // STYLES
@@ -9,21 +10,41 @@ import withNavBarStyles from './NavBar.style';
 // const drawerWidth = 50;
 
 function NavBar({ classes }) {
+  const [open, setOpen] = React.useState(false);
+
+  function handleDrawer() {
+    open ? setOpen(false) : setOpen(true);
+  }
+
   return (
     <div>
-      <Drawer variant="permanent" classes={{ paper: classes.drawer }}>
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx({ [classes.drawer]: true, [classes.drawerOpen]: open, [classes.drawerClose]: !open })
+        }}
+      >
         <div>
-          <IconButton>
-            <Dashboard />
-          </IconButton>
-          <Divider />
-          <IconButton>
-            <DesktopWindows />
-          </IconButton>
-          <Divider />
-          <IconButton>
-            <Work />
-          </IconButton>
+          <List>
+            <ListItem button onClick={handleDrawer}>
+              <ListItemIcon>
+                <Dashboard />
+              </ListItemIcon>
+              <ListItemText primary="Dashboard" />
+            </ListItem>
+            <ListItem button onClick={handleDrawer}>
+              <ListItemIcon>
+                <DesktopWindows />
+              </ListItemIcon>
+              <ListItemText primary="Monitor" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <Work />
+              </ListItemIcon>
+              <ListItemText primary="Task" />
+            </ListItem>
+          </List>
         </div>
       </Drawer>
     </div>

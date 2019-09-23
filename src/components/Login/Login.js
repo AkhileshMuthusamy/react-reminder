19edce399178e1adac1324d7239bfff299c9e9c6
Akhilesh <AkhileshM@outlook.in>
2019-09-23 19:00:47 +0530

    Added styles to element

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,12 +11,21 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  main: {
+    width: '100%',
+    height: '100%',
     backgroundColor: theme.palette.primary.main
   },
-  controls: {
+  loginPage: {
+    padding: 30,
+    backgroundColor: 'white'
+  },
+  buttonContainer: {
+    display: 'flex',
     width: '100%',
-    height: '100%'
+    justifyContent: 'center'
   }
 }));
 
@@ -24,74 +33,83 @@ export default function Login() {
   const classes = useStyles();
   return (
     //   Here clsx package is used to merge two classes
-    <div className={clsx({ [classes.container]: true, [classes.controls]: true })}>
-      <Typography variant="h4">LOGIN</Typography>
-      <Formik
-        initialValues={{ email: 'Ak@Ak.com', password: '' }}
-        validationSchema={Yup.object().shape({
-          email: Yup.string()
-            .email()
-            .required('Email cannot be empty'),
-          password: Yup.string()
-            .required('Password Required')
-            .min(10, 'Must be more than 10 characters')
-        })}
-        onSubmit={(values, formikActions) => {
-          console.log('Form submitted');
-          console.dir(values);
-          formikActions.setSubmitting(false);
-        }}
-      >
-        {props => {
-          const {
-            values,
-            handleChange,
-            handleReset,
-            handleBlur,
-            handleSubmit,
-            errors,
-            dirty,
-            isSubmitting,
-            isValid
-          } = props;
-          return (
-            <form className={classes.container}>
-              <TextField
-                label="Email"
-                name="email"
-                type="email"
-                variant="outlined"
-                margin="normal"
-                value={values.email}
-                onChange={handleChange}
-              />
-              {errors.email && <div>{errors.email}</div>}
-              <TextField
-                label="Password"
-                name="password"
-                type="password"
-                variant="outlined"
-                margin="normal"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={false}
-              />
-              {errors.password && <div>{errors.password}</div>}
-              <Button variant="outlined" onClick={handleReset} disabled={!dirty || isSubmitting}>
-                RESET
-              </Button>
-              <Button variant="contained" onClick={handleSubmit} disabled={!isValid || isSubmitting}>
-                LOGIN
-              </Button>
-              <br />
-              <pre>
-                <strong>props</strong> = {JSON.stringify(props, null, 2)}
-              </pre>
-            </form>
-          );
-        }}
-      </Formik>
+    <div className={clsx({ [classes.container]: true, [classes.main]: true })}>
+      <div className={classes.loginPage}>
+        <Typography variant="h4">LOGIN</Typography>
+        <Formik
+          initialValues={{ email: 'Ak@Ak.com', password: '' }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email()
+              .required('Email cannot be empty'),
+            password: Yup.string()
+              .required('Password Required')
+              .min(10, 'Must be more than 10 characters')
+          })}
+          onSubmit={(values, formikActions) => {
+            console.log('Form submitted');
+            console.dir(values);
+            formikActions.setSubmitting(false);
+          }}
+        >
+          {props => {
+            const {
+              values,
+              handleChange,
+              handleReset,
+              handleBlur,
+              handleSubmit,
+              errors,
+              dirty,
+              isSubmitting,
+              isValid
+            } = props;
+            return (
+              <form className={classes.container}>
+                <TextField
+                  label="Email"
+                  name="email"
+                  type="email"
+                  variant="outlined"
+                  margin="normal"
+                  value={values.email}
+                  onChange={handleChange}
+                />
+                {errors.email && <div>{errors.email}</div>}
+                <TextField
+                  label="Password"
+                  name="password"
+                  type="password"
+                  variant="outlined"
+                  margin="normal"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={false}
+                />
+                {errors.password && <div>{errors.password}</div>}
+                <div className={classes.buttonContainer}>
+                  <Button variant="outlined" color="primary" onClick={handleReset} disabled={!dirty || isSubmitting}>
+                    RESET
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                    disabled={!isValid || isSubmitting}
+                  >
+                    LOGIN
+                  </Button>
+                </div>
+                <br />
+                <pre>
+                  <strong>props</strong> = {JSON.stringify(props, null, 2)}
+                </pre>
+              </form>
+            );
+          }}
+        </Formik>
+      </div>
     </div>
   );
 }

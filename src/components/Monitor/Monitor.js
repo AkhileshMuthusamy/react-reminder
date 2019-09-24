a48f5bb50e27e166767e385003c631892beb7384
Akhilesh <AkhileshM@outlook.in>
2019-09-24 16:49:42 +0530

    Initialized Monitor and Task component

---
+++ src/components/Monitor/Monitor.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Monitor extends Component {
+  render() {
+    return (
+      <div>
+        <p>Monitor</p>
+        <p>
+          This is why understanding Route was so important. Nothing has changed from earlier to right now, but for some
+          reason, your brain gets all worked up since we’re rendering a Route outside of our main App component
+        </p>
+      </div>
+    );
+  }
+}

f0a5a71c1beab5e02af35db17621b4a1da768dd2
Akhilesh <AkhileshM@outlook.in>
2019-10-09 17:48:21 +0530

    Completed the core logic of drag and drop

--- src/components/shared/DragAndDrop/DragAndDrop.js
+++ src/components/shared/DragAndDrop/DragAndDrop.js
@@ -1,12 +1,21 @@
 import React, { createRef, useState } from 'react';
 import './DragAndDrop.css';
+import pretty from 'prettysize';
 import { CloudUpload, Close } from '@material-ui/icons';
 import { Card, CardHeader, IconButton, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { ReactComponent as PDF } from './Icons/pdf.svg';
+import { ReactComponent as JPG } from './Icons/jpg.svg';
+import { ReactComponent as UNKNOWN } from './Icons/unknown.svg';
+import { ReactComponent as PNG } from './Icons/png.svg';
+import { ReactComponent as DOC } from './Icons/doc.svg';
+import { ReactComponent as ZIP } from './Icons/zip.svg';
+import { ReactComponent as GIF } from './Icons/gif.svg';
+import { ReactComponent as TXT } from './Icons/txt.svg';
 
 export default function DragAndDrop({ disabled, onChange }) {
   let fileInputRef = createRef();
+  const [filePreview, setFilePreview] = useState([]);
   const [hightlight, setHightlight] = useState(false);
 
   const openFileDialog = () => {
@@ -18,6 +27,8 @@ export default function DragAndDrop({ disabled, onChange }) {
   const onFilesAdded = event => {
     if (!disabled) {
       const files = fileListToArray(event.target.files);
+      files.push(...filePreview);
+      setFilePreview(files);
       onChange(files);
     }
   };
@@ -45,23 +56,62 @@ export default function DragAndDrop({ disabled, onChange }) {
     event.preventDefault();
     if (!disabled && onChange) {
       const files = fileListToArray(event.dataTransfer.files);
+      files.push(...filePreview);
+      setFilePreview(files);
       onChange(files);
     }
     setHightlight(false);
   };
 
-  const FileCard = () => {
+  const removeFile = index => {
+    const newFiles = [...filePreview];
+    newFiles.splice(index, 1);
+    setFilePreview(newFiles);
+    onChange(newFiles);
+  };
+
+  const DisplayFileTypeIcon = ({ fileType }) => {
+    console.log(fileType);
+    switch (fileType) {
+      case 'image/jpeg': {
+        return <JPG style={{ height: 40, width: 40 }} />;
+      }
+      case 'image/png': {
+        return <PNG style={{ height: 40, width: 40 }} />;
+      }
+      case 'application/pdf': {
+        return <PDF style={{ height: 40, width: 40 }} />;
+      }
+      case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document': {
+        return <DOC style={{ height: 40, width: 40 }} />;
+      }
+      case 'application/zip': {
+        return <ZIP style={{ height: 40, width: 40 }} />;
+      }
+      case 'image/gif': {
+        return <GIF style={{ height: 40, width: 40 }} />;
+      }
+      case 'text/plain': {
+        return <TXT style={{ height: 40, width: 40 }} />;
+      }
+      default: {
+        return <UNKNOWN style={{ height: 40, width: 40 }} />;
+      }
+    }
+  };
+
+  const FileCard = ({ file, index }) => {
     return (
-      <Card>
+      <Card style={{ marginBottom: 5 }}>
         <CardHeader
-          avatar={<PDF style={{ height: 40, width: 40 }} />}
+          avatar={<DisplayFileTypeIcon fileType={file.type} />}
           action={
-            <IconButton aria-label="close" onClick={() => 0}>
+            <IconButton aria-label="close" onClick={() => removeFile(index)}>
               <Close />
             </IconButton>
           }
-          title="File1.pdf"
-          subheader="Size: 2.1 MB"
+          title={file.name}
+          subheader={`Size: ${pretty(file.size)}`}
         />
       </Card>
     );
@@ -78,10 +128,12 @@ export default function DragAndDrop({ disabled, onChange }) {
         onClick={openFileDialog}
       >
         <input ref={fileInputRef} className={'FileInput'} type="file" multiple onChange={onFilesAdded} />
-        <p>Drop your files here or click to add files</p>
+        <Typography variant="body1">Drop your files here or click to add files</Typography>
         <CloudUpload />
-        <FileCard />
       </div>
+      {filePreview.map((file, index) => {
+        return <FileCard key={index} index={index} file={file} />;
+      })}
     </div>
   );
 }
